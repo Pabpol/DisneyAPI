17c87ee97c10fce09c1fb492cd74fc51056672f9
PabPol <pabpolval@gmail.com>
2021-10-04 22:46:47 -0300

    application.properties seteado para la DB local disney_api

---
+++ src/main/java/com/AlkemyChallenge/DinseyAPI/DinseyApiApplication.java
@@ -0,0 +1,13 @@
+package com.AlkemyChallenge.DinseyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinseyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinseyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AlkemyChallenge/DinseyAPI/DinseyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AlkemyChallenge.DinseyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinseyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
